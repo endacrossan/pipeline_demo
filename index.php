71ef02baf2fe0e61fd074d60b41d36b072bd8082
Enda Crossan <48137079+endacrossan@users.noreply.github.com>
2023-07-19 10:12:00 +0100

    Update index.php
    Changed ! to full stop.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>AWS CodePipeline Demo</title>
 </head>
 <body>
-<?php echo '<p>Weather App - quick test for PHP app!</p>'; ?>
+<?php echo '<p>Weather App - quick test for PHP app.</p>'; ?>
 <!-- <?php echo '<br><a href="weather.php">See Weather</a>'; ?> -->
 </body>
 </html>
